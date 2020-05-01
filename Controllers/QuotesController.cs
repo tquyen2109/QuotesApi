e6f8e8893b8ff1dafe32a2f495aebcdbf962cc34
Quyen Tran <t.quyen1997@yahoo.com>
2020-04-30 21:31:43 -0400

    First Commit

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi.Data;
+using QuotesApi.Models;
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+        public QuotesController(QuotesDbContext quotesDbContext)
+        {
+            _quotesDbContext = quotesDbContext;
+        }
+        // GET: api/Quotes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_quotesDbContext.Quotes);
+        }
+
+        // GET: api/Quotes/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if(quote==null)
+            {
+                return NotFound("No record found...");
+            }
+            return Ok(quote);
+         
+        }
+
+        // POST: api/QuotesS
+        [HttpPost]
+        public IActionResult Post([FromBody] Quote quote)
+        {
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT: api/Quotes/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Quote quote)
+        {
+            var entity = _quotesDbContext.Quotes.Find(id);
+            if(entity ==null)
+            {
+                return NotFound("No record found");
+            }
+            else
+            {
+                entity.Title = quote.Title;
+                entity.Author = quote.Author;
+                entity.Description = quote.Description;
+                entity.Type = quote.Type;
+                entity.CreatedAt = quote.CreatedAt;
+                _quotesDbContext.SaveChanges();
+                return Ok("Record update successfully");
+            }
+           
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if (quote == null)
+            {
+                return NotFound("No record found");
+            }
+            else
+            {
+                _quotesDbContext.Quotes.Remove(quote);
+                _quotesDbContext.SaveChanges();
+                return Ok("Deleted");
+
+            }
+
+        }
+    }
+}
---
+++ Models/Quote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotesApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Author { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
